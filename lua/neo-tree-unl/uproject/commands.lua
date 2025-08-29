bc0fdaf97883522e9e4b55738831e99769d268ea
taku25 <taku36@gmail.com>
2025-08-29 16:49:26 +0900

    change component  and components

--- lua/neo-tree-unl/uproject/commands.lua
+++ lua/neo-tree-unl/uproject/commands.lua
@@ -1,2 +1,4 @@
 --This file should contain all commands meant to be used by mappings.
-return require("neo-tree.sources.filesystem.commands")
+local common_components = require("neo-tree.sources.common.commands")
+
+return common_components
